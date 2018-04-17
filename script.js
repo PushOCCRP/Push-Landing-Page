37ced6d6116f5888549ed63b0f0c450be09e1d58
Mlhworkspace <mlhworkspace@gmail.com>
2018-04-17 17:16:48 -0500

    scripttest

---
+++ script.js
@@ -0,0 +1,15 @@
+window.onscroll = function () {sticky_nav}
+
+nav = document.querySelector('#navigation');
+navTop = nav.offsetTop;
+
+function sticky_nav() {
+  if (window.pageYOffset >= navTop) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    nav.classList.add("sticky");
+  }
+  else {
+    document.body.style.paddingTop = 0;
+    nav.classList.remove("sticky");
+  }
+}
